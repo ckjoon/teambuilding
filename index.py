9d5ae2e9b1048eefdfdc7bf43e3304cca77e1343
Lance Hasson <lance.hasson@gmail.com>
2016-09-24 18:14:41 -0400

    added form functionality and fixed links on the site

--- index.py
+++ index.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('signin.html')
+
+@app.route("/prof_home")
+def prof_home():
+    return render_template('prof_home.html')
+
+@app.route("/student_home")
+def student_home():
+    return render_template('student_home.html')
+
+@app.route("/api/login", methods=['POST'])
+def login():
+    #handle login stuff.
+    if request.method == 'POST':
+        if request.form.get('gtusername') != "jchoi302":
+            return redirect(url_for('student_home'))
+        else:
+            return redirect(url_for('prof_home'))
+    
 
 if __name__ == "__main__":
     app.run()
