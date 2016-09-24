6659196ec68409ceefc8f74faf26b032c76b555a
Lance Hasson <lance.hasson@gmail.com>
2016-09-24 19:58:54 -0400

    Starting postgres setup

--- index.py
+++ index.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_cas import CAS, login_required
 app = Flask(__name__)
+CAS(app, '/api/cas/') # this adds the prefix '/api/cas/' to the /login and /logout
+                      # routes that CAS provides
+                      
+app.config['CAS_SERVER'] = 'https://login.gatech.edu/cas/'
+app.config['CAS_AFTER_LOGIN'] = 'student_home' # CHANGE THIS: needs to redirect to somehow determine
+                                               # what kind of person is logging in.
 
 @app.route("/")
 def main():
     return render_template('signin.html')
 
 @app.route("/prof_home")
+@login_required
 def prof_home():
     return render_template('prof_home.html')
 
 @app.route("/student_home")
+@login_required
 def student_home():
     return render_template('student_home.html')
 
