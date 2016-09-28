2d2344b8e49f28cb8b0edaffb0b58d03440c8712
Lance Hasson <lance.hasson@gmail.com>
2016-09-27 23:20:23 -0400

    updated team model

--- teambuildingapp/models.py
+++ teambuildingapp/models.py
@@ -21,6 +21,13 @@ class Team(db.Model):
     team_name = db.Column(db.String(120), unique=True)
     team_leader = db.Column(db.Integer, unique=True)
 
+    def __init__(self, team_name, team_leader):
+        self.team_name = team_name
+        self.team_leader = team_leader
+
+    def __repr__(self):
+        return '<User %r>' % self.team_name
+
 
 # class Roster(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
